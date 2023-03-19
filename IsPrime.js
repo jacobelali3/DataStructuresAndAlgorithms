2a108e7032d28a9adbc17d82c6445892d8c256cd
Jacob <jacobelali3@outlook.com>
2023-03-19 23:16:54 +1100

    added special case to IsPrime

--- IsPrime.js
+++ IsPrime.js
@@ -1,5 +1,5 @@
 const isPrime = (n) => {
-    if(n == 0 || n == 1) return false;
+    if(n <2 ) return false;
 
     for(let i = 2; i <= Math.sqrt(n); i++){
       if(n % i === 0){
@@ -12,3 +12,4 @@ const isPrime = (n) => {
   //time = o sqrt(n) because we're only checking between range of 2 <-> sqrt(n),
   //we do this in order to avoid checking for duplicate values, which speeds up the code abit better than linear time
   //space = o(1) because were only storing one variable i, its constant.
+  //we check for special cases 0 & 1 and negative numbers (n < 2) because they can't be prime
